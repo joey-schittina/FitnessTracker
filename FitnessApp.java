b6a8da7068091c9dc8ddf39c5849fe49610e9b5d
Joseph Schittina <joseph.schittina@uconn.edu>
2022-01-07 23:08:29 -0500

    Rough Draft
    Limited Functionality

--- FitnessApp.java
+++ FitnessApp.java
@@ -19,7 +19,9 @@
 
         static String day;
         static String month;
+        static int monthInt;
         static String year;
+        static String fulldate;
         static Color myBlue = new Color(0,100,200);
 
         public static void main(String[] args){
@@ -75,10 +77,28 @@
             start.addActionListener(new ActionListener() {
                 
                 public void actionPerformed(ActionEvent e) {
+
                     
                     day=dayBox.getSelectedItem().toString();
                     month=monthBox.getSelectedItem().toString();
                     year=yearBox.getSelectedItem().toString();
+                    monthInt = monthBox.getSelectedIndex()+1;
+                    fulldate = year+"-"+monthInt+"-"+day;
+                    System.out.println(fulldate);
+
+                    try{    
+                        Connection con=DriverManager.getConnection(  
+                        "jdbc:mysql://localhost:3306/fitness_app","root","Slapshot1515");  
+                        Statement stmt=con.createStatement(); 
+                        ResultSet rs = stmt.executeQuery("select date from dates where date=\""+fulldate+"\"");
+                        rs.next();
+                        if (rs.getRow()==0){
+                            int result = stmt.executeUpdate("insert into dates values(\""+fulldate+"\",null,null,null)");
+                        }
+                        con.close();  
+                        }catch(Exception e2){ System.out.println(e);}
+
+
 
                     JFrame mainFrame = new JFrame();
                     mainFrame.setTitle("PirateJoe13's Fitness Tracker");
@@ -97,8 +117,8 @@
                     myDate.setFont(new Font("Comic Sans", Font.ROMAN_BASELINE,20));
                     myDate.setForeground(Color.white);
 
-                    JButton addMeal = new JButton("Add Meal");
-                    JButton newInfo = new JButton("Input New Nutritional Info");
+                    JButton addMeal = new JButton("Log a meal");
+                    JButton newInfo = new JButton("Input New Food Info");
                     JButton todaysMeals = new JButton("View Today's Nutritional Info");
 
                     todaysMeals.setBounds(150,150,200,30);
@@ -166,6 +186,193 @@
 
                             nutFrame.add(nutPanel);
                             nutFrame.setVisible(true);
+
+                        }
+                    });
+
+                    addMeal.addActionListener(new ActionListener() {
+                
+                        public void actionPerformed(ActionEvent e) {
+
+                            JFrame inpFrame = new JFrame();
+                            inpFrame.setTitle("PirateJoe13's Fitness Tracker");
+                            inpFrame.setResizable(false);
+                            inpFrame.setLayout(null);
+                            inpFrame.setSize(500,600);
+                                
+                            JPanel inpPanel = new JPanel();
+                            inpPanel.setBackground(myBlue);
+                            inpPanel.setBounds(0,0,500,270);
+                            inpPanel.setLayout(null);
+
+                            JLabel guide2 = new JLabel("List of Foods in Database:");
+                            guide2.setBounds(50,230,400,50);
+                            guide2.setHorizontalAlignment(JLabel.CENTER);
+                            guide2.setFont(new Font("Comic Sans", Font.ROMAN_BASELINE,20));
+                            guide2.setForeground(Color.white);
+                            inpPanel.add(guide2);
+
+                            JLabel guide = new JLabel("Input Food and Time Eaten:");
+                            guide.setBounds(50,50,400,50);
+                            guide.setHorizontalAlignment(JLabel.CENTER);
+                            guide.setFont(new Font("Comic Sans", Font.ROMAN_BASELINE,20));
+                            guide.setForeground(Color.white);
+
+                            JTextField name = new JTextField("Food Eaten:");
+                            JTextField mealTime = new JTextField("Time Eaten:");
+                            JButton enter = new JButton("Submit");
+
+                            enter.setBounds(200,200,100,30);
+                            name.setBounds(100,150,150,30);
+                            mealTime.setBounds(275,150,150,30);
+
+                            JPanel itemsPanel = new JPanel();
+                            itemsPanel.setBackground(myBlue);
+                            itemsPanel.setBounds(0,270,500,330);
+                            itemsPanel.setLayout(new GridLayout(20,5));
+                            
+
+                            try{
+
+                                
+                                Connection con=DriverManager.getConnection(  
+                                "jdbc:mysql://localhost:3306/fitness_app","root","Slapshot1515");  
+            
+                                Statement stmt=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE); 
+                                ResultSet rs = stmt.executeQuery("select * from foods");
+                                rs.last();
+                                int rows=rs.getRow();
+                                rs.first();
+                                int newrows=(int)Math.ceil(rows/2.0);
+                                System.out.println(newrows);
+                                itemsPanel.setLayout(new GridLayout(newrows,2));
+                                JLabel items[] = new JLabel[rows];
+                                for(int i=0; i<rows; i++){
+                                    items[i]=new JLabel(rs.getString(1)+" is/are "+Integer.toString(rs.getInt(2))+" calories.");
+                                    items[i].setForeground(Color.white);
+                                    itemsPanel.add(items[i]);
+                                    rs.next();
+                                }
+                                
+
+
+
+                            }catch(Exception e2){ System.out.println(e);}
+                            
+                            inpPanel.add(enter);
+                            inpPanel.add(name);
+                            inpPanel.add(mealTime);
+                            inpPanel.add(guide);
+
+                            enter.addActionListener(new ActionListener() {
+                
+                                public void actionPerformed(ActionEvent e) {
+
+                                    String foodsString = name.getText();
+                                    String timeString = mealTime.getText();
+                                    System.out.println(foodsString);
+                                    System.out.println(timeString);
+                                    System.out.println(fulldate);
+                                    try{
+
+                                
+                                        Connection con=DriverManager.getConnection(  
+                                        "jdbc:mysql://localhost:3306/fitness_app","root","Slapshot1515");  
+                    
+                                        Statement stmt=con.createStatement(); 
+                                        int res = stmt.executeUpdate("insert into meals values(\""+fulldate+"\",\""+foodsString+"\",\""+timeString+"\")");
+
+
+                                    }catch(Exception e2){ System.out.println(e);}
+                                }
+                            });
+                            inpFrame.add(inpPanel);
+                            inpFrame.setVisible(true);
+                            inpFrame.add(itemsPanel);
+                            itemsPanel.setVisible(true);
+
+                  
+
+                        }
+                    });
+
+
+
+                    todaysMeals.addActionListener(new ActionListener() {
+                
+                        public void actionPerformed(ActionEvent e) {
+
+                            try{    
+                                Connection con=DriverManager.getConnection(  
+                                "jdbc:mysql://localhost:3306/fitness_app","root","Slapshot1515");  
+                                Statement stmt=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE); 
+                                ResultSet rs = stmt.executeQuery("select * from meals where date=\""+fulldate+"\"");
+
+                                rs.last();
+                                int rows = rs.getRow();
+                                rs.first();
+
+
+
+                                JFrame todaysFrame = new JFrame();
+                                todaysFrame.setTitle("PirateJoe13's Fitness Tracker");
+                                todaysFrame.setResizable(false);
+                                todaysFrame.setLayout(null);
+                                todaysFrame.setSize(500,600);
+                                todaysFrame.setBackground(myBlue);
+                                    
+                                JPanel titlePanel = new JPanel();
+                                titlePanel.setBackground(myBlue);
+                                titlePanel.setBounds(0,0,500,100);
+                                titlePanel.setLayout(null);
+
+                                JPanel listPanel = new JPanel();
+                                listPanel.setBackground(myBlue);
+                                listPanel.setBounds(0,100,500,20*rows);
+                                listPanel.setLayout(new GridLayout(rows+1,2));
+
+                                JPanel excessPanel = new JPanel();
+                                excessPanel.setBackground(myBlue);
+                                excessPanel.setBounds(0,100+20*rows,500,500-20*rows);
+                                excessPanel.setLayout(new GridLayout(rows+1,2,10,40));
+
+                                JLabel titleLabel = new JLabel("Meals Eaten Today");
+                                titleLabel.setBounds(50,20,400,50);
+                                titleLabel.setHorizontalAlignment(JLabel.CENTER);
+                                titleLabel.setFont(new Font("Comic Sans", Font.ROMAN_BASELINE,20));
+                                titleLabel.setForeground(Color.white);
+
+                                JLabel[] mealArr = new JLabel[rows+1];
+                                JLabel[] timeArr = new JLabel[rows+1];
+
+                                mealArr[0]=new JLabel("List of Items Eaten Today:");
+                                timeArr[0]=new JLabel("Time Meal Was Eaten:");
+
+                                mealArr[0].setForeground(Color.white);
+                                timeArr[0].setForeground(Color.white);
+
+                                listPanel.add(mealArr[0]);
+                                listPanel.add(timeArr[0]);
+
+                                for(int i=0;i<rows;i++){
+                                    mealArr[i+1]=new JLabel(rs.getString(2));
+                                    timeArr[i+1]=new JLabel(rs.getString(3));
+                                    //mealArr[i+1].setSize(50,60);
+                                    //timeArr[i+1].setSize(50,60);
+                                    listPanel.add(mealArr[i+1]);
+                                    listPanel.add(timeArr[i+1]);
+                                    rs.next();
+                                }
+                                
+                                titlePanel.add(titleLabel);
+                    
+
+                                todaysFrame.add(excessPanel);
+                                todaysFrame.add(listPanel);
+                                todaysFrame.add(titlePanel);
+                                todaysFrame.setVisible(true);
+                                con.close();
+                            }catch(Exception e2){ System.out.println(e);}
 
                         }
                     });
